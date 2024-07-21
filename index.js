3607d2fe4d1a99f21544c2dc9181d4672710555c
Dannydramo <alumanduddaniel217@gmail.com>
2024-07-20 17:21:50 -0700

    feat: added cors option

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const globalErrorhandler = require('./controllers/error.controller');
 const routes = require('./routes/index');
 
 const app = express();
+const corsOptions = {
+    origin: ['http://localhost:3000'],
+    optionSuccessStatus: 200,
+    credentials: true,
+};
 app.use(express.json());
+app.use(cors(corsOptions));
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -14,8 +20,8 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('combined'));
 }
 app.use('/api/v1', routes);
-app.get("/", (req, res) => {
-    res.send("Day Creations Backend server is live");
+app.get('/', (req, res) => {
+    res.send('Day Creations Backend server is live');
 });
 app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
